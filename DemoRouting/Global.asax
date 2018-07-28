997a4a477cdaf1493eae42a15b2784edf611b59f
VTS2018 <itmaper@gmail.com>
2018-07-28 08:46:08 +0800

    New

---
+++ DemoRouting/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="DemoRouting.Global" Language="C#" %>
---
+++ DemoRouting/MyPage.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Routing;
+
+namespace DemoRouting
+{
+    public class MyPage : IHttpHandler
+    {
+        public RequestContext RequestContext
+        {
+            get;
+            private set;
+        }
+
+        public MyPage(RequestContext context)
+        {
+            this.RequestContext = context;
+        }
+
+        #region IHttpHandler 成员
+
+        public virtual void ProcessRequest(HttpContext context)
+        {
+            string path = RequestContext.RouteData.Values["page"].ToString().Replace("_", "/") + ".aspx";
+
+            context.Server.Execute(path);
+        }
+
+        public bool IsReusable
+        {
+            get { return false; }
+        }
+        #endregion
+    }
+
+    public class MyRouteHandler : IRouteHandler
+    {
+        #region IRouteHandler 成员
+
+        public IHttpHandler GetHttpHandler(RequestContext requestContext)
+        {
+            return new MyPage(requestContext);
+        }
+        #endregion
+    }
+}
